c4ffcbc8dc0e4927a013bd1fa8cc75860daad4e1
Alexandr <kb_299@bk.ru>
2023-01-27 16:53:10 +0300

    db created

---
+++ src/main/java/ru/gb/web_market/entities/Manufacturer.java
@@ -0,0 +1,35 @@
+package ru.gb.web_market.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "manufacturer")
+@NoArgsConstructor
+public class Manufacturer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "info")
+    private String info;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
